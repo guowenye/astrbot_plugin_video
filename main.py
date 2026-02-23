ae8f057c83dfe75bf1f44d5fe27dfbd2ee45ba8b
Nuclear0709 <agp05432@gmail.com>
2026-02-24 05:50:38 +0800

    訊息組建解析似乎有點 調整為穩定版

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class DwoVideoPlugin(Star):
                 message_chain = [
                     Plain("视频获取成功！") ,
                     video_component,
-                    Plain(f"\u200b\n视频链接：\u200b{video_url}")
+                    Plain(f"视频链接：{video_url}")
                    
                 ]
 
