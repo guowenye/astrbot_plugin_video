4be005f0111795c9d2ec599dec5f3db7a427f399
Nuclear0709 <agp05432@gmail.com>
2026-02-24 05:20:35 +0800

    最終結算修改 應該可以合併用

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # astrbot_plugin_video
 astrbot小姐姐视频插件
 用法：向机器人发送/video或者/小视频
+
+> [!WARNING]
+> 該服務依賴於 xxapi.cn 需註冊取得`API_KEY`
+>
+> 目前API接口已要求要`API_KEY`
+
+![展示圖](Docs/Preview.png)
--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class DwoVideoPlugin(Star):
                 message_chain = [
                     Plain("视频获取成功！") ,
                     video_component,
-                    Plain(f"视频链接：{video_url}")
+                    Plain(f"\u200b\n视频链接：{video_url}\u200b")
                    
                 ]
 
